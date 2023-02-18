55a0677ad8443a6563a638a87e0024f9af87871a
Evgeny Eroshin <eee333@yandex.ru>
2023-02-18 15:19:48 +0300

    Create views and serializer for Author

--- books_library/urls.py
+++ books_library/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from service.views import AuthorListView, AuthorDetailView, AuthorUpdateView, AuthorDeleteView, AuthorCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('author/', AuthorListView.as_view()),
+    path('author/<int:pk>', AuthorDetailView.as_view()),
+    path('author/update/<int:pk>', AuthorUpdateView.as_view()),
+    path('author/delete/<int:pk>', AuthorDeleteView.as_view()),
+    path('author/create/', AuthorCreateView.as_view()),
 ]
--- service/views.py
+++ service/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, CreateAPIView
+from django.views import View
 
-# Create your views here.
+from service.models import Author
+from service.serializers import AuthorSerializer
+
+
+class AuthorListView(ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetailView(RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AuthorUpdateView(UpdateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AuthorDeleteView(DestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorCreateView(CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
