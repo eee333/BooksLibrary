4e636f77c5bc20cc357a685289c2c4a6d6467e2d
Evgeny Eroshin <eee333@yandex.ru>
2023-03-09 01:31:30 +0300

    Add author as link in admin book

--- service/admin.py
+++ service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django.urls import reverse
+from django.utils.html import format_html
 
 from service.models import Author, Book, Customer
 
@@ -9,7 +10,14 @@ class AuthorAdmin(admin.ModelAdmin):
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'pages_count', 'books_count', 'created_at')
+    list_display = ('name', 'author_link', 'pages_count', 'books_count', 'created_at')
+
+    def author_link(self, obj):
+        author = obj.author
+        url = reverse('admin:service_author_changelist') + str(author.pk)
+        return format_html(f'<a href="{url}">{author}</a>')
+
+    author_link.short_description = 'Автор'
 
 
 class CustomerAdmin(admin.ModelAdmin):
